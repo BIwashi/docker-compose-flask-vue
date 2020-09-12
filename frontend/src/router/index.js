4218b49384088c22c4ceb30115004ed3b8d5a69a
Shota Iwami <saxo-folatre1129@outlook.jp>
2020-09-12 18:23:40 +0900

    finished https://qiita.com/kasayu/items/43734e570dc458a5be15

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,17 +1,23 @@
-import Vue from "vue";
-import Router from "vue-router";
-import HelloWorld from "@/components/HelloWorld";
-import Home from "@/components/Home"; // ★★★追加★★★
-Vue.use(Router);
+import Vue from 'vue'
+import Router from 'vue-router'
+import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
+
+Vue.use(Router)
 
 export default new Router({
-  mode: "history", // ★★★追加★★★
+  mode: 'history', 
   routes: [
     {
-      // '/'に対して、frontend/src/components/ 配下のHelloWorld.vueファイルを参照する
-      path: "/",
-      name: "HelloWorld",
+      path: '/',
+      name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      // '/home'に対して、frontend/src/components/ 配下のHome.vueファイルを参照する
+      path: '/home',
+      name: 'Home',
+      component: Home
     }
   ]
-});
+})
